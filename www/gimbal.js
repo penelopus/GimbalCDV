f38535d542785ed436f2050c53d5d2c954afc8db
Julie Driscoll <julie.driscoll@gmail.com>
2014-07-29 17:22:10 -0700

    added argument to empty array, reference in comments to composition of call

--- www/gimbal.js
+++ www/gimbal.js
@@ -4,7 +4,8 @@ var PLUGIN_ID = "GimbalCDV";
 
 var Gimbal = {
 	doGimbal: function() {
-		cordova.exec(function() {}, function(error) {}, "Gimbal", "gimbalMethod", []);
+		//exec(<successFunction>, <failFunction>, <service>, <action>, [<args>]);
+		cordova.exec(function(res) { console.log('success'); console.log('res');}, function(error) { console.log('error'); console.log(error); }, "Gimbal", "gimbalMethod", ['testjd']);
 	}
 }
 
